a29df328a0e117ef0fff5e51e2c5907547f9c583
Makoto Uju <ainehanta@users.noreply.github.com>
2016-11-07 18:21:58 +0900

    Refactor

--- SHT2x.cpp
+++ SHT2x.cpp
@@ -11,101 +11,113 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+uint8_t SHT2x::readUserRegistor(void) {
+  Wire.beginTransmission(this->ADDR);
+  Wire.write(static_cast<uint8_t>(Command::ReadUserRegistor));
+  Wire.endTransmission();
+
+  Wire.requestFrom((int)this->ADDR, 1);
+  while(!Wire.available()) {}
+  return Wire.read();
+}
+
+uint8_t SHT2x::writeUserRegistor(uint8_t value) {
+  Wire.beginTransmission(this->ADDR);
+  Wire.write(static_cast<uint8_t>(Command::WriteUserRegistor));
+  Wire.write(value);
+  Wire.endTransmission();
+
+  Wire.requestFrom((int)this->ADDR, 1);
+  while(!Wire.available()) {}
+
+  return value == Wire.read();
+}
+
+uint16_t SHT2x::readValue(uint8_t trigger) {
+  uint8_t msb, lsb, crc;
+
+  Wire.beginTransmission(this->ADDR);
+  Wire.write(trigger);
+  Wire.endTransmission();
+
+  Wire.requestFrom((int)this->ADDR, 3);
+  while(!Wire.available()) {}
+  msb = Wire.read();
+  lsb = Wire.read();
+  crc = Wire.read();
+
+  return msb << 8 | lsb;
+}
+
 void SHT2x::begin(void){
   reset();
 }
 
 void SHT2x::reset(void){
   Wire.beginTransmission(this->ADDR);
-  Wire.write(0xFE);
+  Wire.write(static_cast<uint8_t>(Command::SoftReset));
   Wire.endTransmission();
   delay(15);
 }
 
 float SHT2x::readTemperature(uint16_t timeout_ms=SHT2x_DEFAULT_TIMEOUT_MS) {
-  uint8_t recv_packet[3];
-  uint16_t st;
-
-  Wire.beginTransmission(this->ADDR);
-  Wire.write(0b11100011);
-  Wire.endTransmission();
-
-  Wire.requestFrom((int)this->ADDR, 3);
-  while(!Wire.available()) {}
-  recv_packet[0] = Wire.read();
-  recv_packet[1] = Wire.read();
-  recv_packet[2] = Wire.read();
-
-  st = recv_packet[0] << 8 | recv_packet[1];
+  uint16_t st = readValue(static_cast<uint8_t>(Command::TemperatureHold));
+  st &= ~(0b11U);
 
   return convertStToTemperature(st);
 }
 
 float SHT2x::readHumidity(uint16_t timeout_ms=SHT2x_DEFAULT_TIMEOUT_MS) {
-  uint8_t recv_packet[3];
-  uint16_t srh;
-
-  Wire.beginTransmission(this->ADDR);
-  Wire.write(0b11100101);
-  Wire.endTransmission();
-
-  Wire.requestFrom((int)this->ADDR, 3);
-  while(!Wire.available()) {}
-  recv_packet[0] = Wire.read();
-  recv_packet[1] = Wire.read();
-  recv_packet[2] = Wire.read();
-
-  srh = recv_packet[0] << 8 | recv_packet[1];
+  uint16_t srh = readValue(static_cast<uint8_t>(Command::HumidityHold));
+  srh &= ~(0b11U);
 
   return convertSrhToHumidity(srh);
 }
 
 bool SHT2x::heaterOn(void) {
-  uint8_t reg;
-  uint8_t res;
+  uint8_t value;
 
-  Wire.beginTransmission(this->ADDR);
-  Wire.write(0b11100111);
-  Wire.endTransmission();
-
-  Wire.requestFrom((int)this->ADDR, 1);
-  while(!Wire.available()) {}
-  reg = Wire.read();
-  bitSet(reg, 2);
-
-  Wire.beginTransmission(this->ADDR);
-  Wire.write(0b11100110);
-  Wire.write(reg);
-  Wire.endTransmission();
-
-  Wire.requestFrom((int)this->ADDR, 1);
-  while(!Wire.available()) {}
-  res = Wire.read();
-
-  return res == reg;
+  value = readUserRegistor();
+  bitSet(value, 2);
+  return value == writeUserRegistor(value);
 }
 
 bool SHT2x::heaterOff(void) {
-  uint8_t reg;
-  uint8_t res;
+  uint8_t value;
 
-  Wire.beginTransmission(this->ADDR);
-  Wire.write(0b11100111);
-  Wire.endTransmission();
+  value = readUserRegistor();
+  bitClear(value, 2);
+  return value == writeUserRegistor(value);
+}
 
-  Wire.requestFrom((int)this->ADDR, 1);
-  while(!Wire.available()) {}
-  reg = Wire.read();
-  bitClear(reg, 2);
+bool SHT2x::otpReloadOff(void) {
+  uint8_t value;
 
-  Wire.beginTransmission(this->ADDR);
-  Wire.write(0b11100110);
-  Wire.write(reg);
-  Wire.endTransmission();
+  value = readUserRegistor();
+  bitSet(value, 1);
+  return value == writeUserRegistor(value);
+}
 
-  Wire.requestFrom((int)this->ADDR, 1);
-  while(!Wire.available()) {}
-  res = Wire.read();
+bool SHT2x::otpReloadOn(void) {
+  uint8_t value;
 
-  return res == reg;
+  value = readUserRegistor();
+  bitClear(value, 1);
+  return value == writeUserRegistor(value);
+}
+
+bool SHT2x::isEOD(void) {
+  uint8_t value;
+
+  value = readUserRegistor();
+  return bitRead(value, 6);
+}
+
+bool SHT2x::setResolution(Resolution resolution) {
+  uint8_t value;
+
+  value = readUserRegistor();
+  value &= ~0b10000001;
+  value |= static_cast<uint8_t>(resolution);
+  return value == writeUserRegistor(value);
 }
--- SHT2x.h
+++ SHT2x.h
@@ -14,6 +14,18 @@
 
 class SHT2x {
   private:
+    const uint8_t ADDR = 0b1000000;
+
+    enum class Command : uint8_t {
+      TemperatureHold   = 0b11100011,
+      HumidityHold      = 0b11100101,
+      TemperatureUnHold = 0b11110011,
+      HumidityUnHold    = 0b11110101,
+      WriteUserRegistor = 0b11100110,
+      ReadUserRegistor  = 0b11100111,
+      SoftReset         = 0b11111110
+    };
+
     inline float convertStToTemperature(uint16_t st) {
       return -46.85 + 175.72 * (float)st / pow(2, 16);
     }
@@ -22,19 +34,35 @@ class SHT2x {
       return -6.0 + 125.0 * (float)srh / pow(2, 16);
     }
 
+    uint8_t readUserRegistor(void);
+    uint8_t writeUserRegistor(uint8_t value);
+    uint16_t readValue(uint8_t trigger);
+    // readValueAsync
+
   public:
-    const uint8_t ADDR = 0b1000000;
+
+    enum class Resolution : uint8_t {
+      RH_12_T_14 = 0b00000000,
+      RH_8_T_12  = 0b00000001,
+      RH_10_T_13 = 0b10000000,
+      RH_11_T_11 = 0b10000001
+    };
+
     void begin(void);
     void reset(void);
 
     float readTemperature(uint16_t timeout_ms=SHT2x_DEFAULT_TIMEOUT_MS);
     float readHumidity(uint16_t timeout_ms=SHT2x_DEFAULT_TIMEOUT_MS);
 
+    bool setResolution(Resolution resolution);
+
     bool heaterOn(void);
     bool heaterOff(void);
 
-    // float readTemperatureRequest(void) = 0;
-    // float readHumidityRequest(void) = 0;
+    bool otpReloadOff(void);
+    bool otpReloadOn(void);
+
+    bool isEOD(void);
 };
 
 #endif
